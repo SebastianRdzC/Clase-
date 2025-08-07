f8e734c04234a3b5d0228c3d7c4cb5f30f4815b6
SebastianRdzC <rodriguezcervat3sseb4stian@gmail.com>
2025-08-06 20:46:16 -0600

    Commit para un deploy unico del front, olvide quitar un icono que no usaba

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
 import { Button } from './components/ui/button'
 import { Input } from './components/ui/input'
-import { GraduationCap, Atom } from 'lucide-react';
+import { Atom } from 'lucide-react';
 import './App.css'
 
 function App() {
